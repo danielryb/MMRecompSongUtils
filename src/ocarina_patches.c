991b76a430a6b4d07b43b8c4010d3a35849554be
danielryb <59661841+danielryb@users.noreply.github.com>
2025-05-04 17:24:02 +0200

    change free play fix from force patch to hook

--- src/ocarina_patches.c
+++ src/ocarina_patches.c
@@ -1,5 +1,4 @@
-#include "modding.h"
-#include "global.h"
+#include "song_utils.h"
 
 // #include "audio/code_8019AF00.c"
 
@@ -18,29 +17,22 @@ extern u32 sOcarinaAvailableSongFlags;
 extern u8 sButtonToPitchMap[5];
 extern u8 sPlayedOcarinaSongIndexPlusOne;
 extern u8 sIsOcarinaInputEnabled;
+extern u8 sMusicStaffPrevPitch;
 
 void AudioOcarina_StartDefault(u32 ocarinaFlags);
 
 void AudioOcarina_CheckIfStartedSong(void);
 void AudioOcarina_UpdateCurOcarinaSong(void);
 
-/**
- * Checks for ocarina songs from user input with no music staff prompt.
- * Includes ocarina actions such as free play, no warp
- */
-RECOMP_FORCE_PATCH void AudioOcarina_CheckSongsWithoutMusicStaff(void) {
-    u32 pitch;
-    u8 ocarinaStaffPlayingPosStart;
-    u8 songIndex;
-    u8 j;
-    u8 k;
+bool L_pressed = false;
+bool skipped_AudioOcarina_CheckSongsWithoutMusicStaff = false;
+u8 sCurOcarinaPitch_tmp = OCARINA_PITCH_NONE;
 
-    // @mod Fix instrument animation not playing if L is held.
-    // if (CHECK_BTN_ANY(sOcarinaInputButtonCur, BTN_L) &&
-    //     CHECK_BTN_ANY(sOcarinaInputButtonCur, BTN_A | BTN_CRIGHT | BTN_CLEFT | BTN_CDOWN | BTN_CUP)) {
-    //     AudioOcarina_StartDefault(sOcarinaFlags);
-    //     return;
-    // }
+RECOMP_HOOK("AudioOcarina_CheckSongsWithoutMusicStaff") void on_AudioOcarina_CheckSongsWithoutMusicStaff(void) {
+    L_pressed = CHECK_BTN_ANY(sOcarinaInputButtonCur, BTN_L);
+    if (L_pressed) {
+        sOcarinaInputButtonCur &= ~BTN_L;
+    }
 
     AudioOcarina_CheckIfStartedSong();
 
@@ -48,54 +40,42 @@ RECOMP_FORCE_PATCH void AudioOcarina_CheckSongsWithoutMusicStaff(void) {
         return;
     }
 
-
-    ocarinaStaffPlayingPosStart = sOcarinaStaffPlayingPos;
     if ((sPrevOcarinaPitch != sCurOcarinaPitch) && (sCurOcarinaPitch != OCARINA_PITCH_NONE)) {
-        sOcarinaStaffPlayingPos++;
-
-        if (sOcarinaStaffPlayingPos > ARRAY_COUNT(sCurOcarinaSongWithoutMusicStaff)) {
-            sOcarinaStaffPlayingPos = 1;
-        }
-
         // @mod Fix instrument animation not playing if L is held.
-        if (CHECK_BTN_ANY(sOcarinaInputButtonCur, BTN_L)) {
+        if (L_pressed) {
+            sOcarinaStaffPlayingPos++;
+
+            if (sOcarinaStaffPlayingPos > ARRAY_COUNT(sCurOcarinaSongWithoutMusicStaff)) {
+                sOcarinaStaffPlayingPos = 1;
+            }
+
             u8 tmp = sOcarinaStaffPlayingPos;
             AudioOcarina_StartDefault(sOcarinaFlags);
             sOcarinaStaffPlayingPos = tmp;
-            return;
+
+            // @mod Force the hooked function to go into the second return.
+            sCurOcarinaPitch_tmp = sCurOcarinaPitch;
+            sCurOcarinaPitch = OCARINA_PITCH_NONE;
+
+            sOcarinaHasStartedSong = false;
+
+            skipped_AudioOcarina_CheckSongsWithoutMusicStaff = true;
         }
+    }
+}
 
-        AudioOcarina_UpdateCurOcarinaSong();
+RECOMP_HOOK_RETURN("AudioOcarina_CheckSongsWithoutMusicStaff") void after_AudioOcarina_CheckSongsWithoutMusicStaff(void) {
+    if (L_pressed) {
+        sOcarinaInputButtonCur |= BTN_L;
+    }
 
-        if ((ABS_ALT(sCurOcarinaBendIndex) > 20) && (ocarinaStaffPlayingPosStart != sOcarinaStaffPlayingPos)) {
-            sCurOcarinaSongWithoutMusicStaff[sOcarinaWithoutMusicStaffPos - 1] = OCARINA_PITCH_NONE;
-        } else {
-            sCurOcarinaSongWithoutMusicStaff[sOcarinaWithoutMusicStaffPos - 1] = sCurOcarinaPitch;
-        }
+    if (skipped_AudioOcarina_CheckSongsWithoutMusicStaff) {
+        sCurOcarinaPitch = sCurOcarinaPitch_tmp;
+        sOcarinaHasStartedSong = true;
 
-
-        // This nested for-loop tests to see if the notes from the ocarina are identical
-        // to any of the songIndex from sFirstOcarinaSongIndex to sLastOcarinaSongIndex
-
-        // Loop through each of the songs
-        for (songIndex = sFirstOcarinaSongIndex; songIndex < sLastOcarinaSongIndex; songIndex++) {
-            // Checks to see if the song is available to be played
-            if ((u32)sOcarinaAvailableSongFlags & (1 << songIndex)) {
-                // Loops through all possible starting indices?
-                // Loops through the notes of the song?
-                for (j = 0, k = 0; (j < gOcarinaSongButtons[songIndex].numButtons) && (k == 0) &&
-                                   (sOcarinaWithoutMusicStaffPos >= gOcarinaSongButtons[songIndex].numButtons);) {
-
-                    pitch = sCurOcarinaSongWithoutMusicStaff[(sOcarinaWithoutMusicStaffPos -
-                                                              gOcarinaSongButtons[songIndex].numButtons) +
-                                                             j];
-
-                    if (pitch == sButtonToPitchMap[gOcarinaSongButtons[songIndex].buttonIndex[j]]) {
-                        j++;
-                    } else {
-                        k++;
-                    }
-                }
+        skipped_AudioOcarina_CheckSongsWithoutMusicStaff = false;
+    }
+}
 
 extern u8 sOcarinaDropInputTimer;
 
